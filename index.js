18b072b3e4127ad10c7828ae6df4bc1797aea513
Shawn Smith <sds.smith24@gmail.com>
2022-03-26 10:32:53 -0400

    declare variables in index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+
+const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const num = "0123456789";
+const special = "!@#$%^&*_-+=";
+
+const minLength = document.getElementById('min-length').value
+const maxLength = document.getElementById('max-length').value
+const includeNum = document.getElementById('include-num').value
+const includeSpec = document.getElementById('include-spec').value
+
+const pwLength = (minLength, maxLength) => Math.floor(Math.random() * (max - min + 1)) + min
+
+let password = ''

b66fe722b74e17fc7ad9d95ff7e3739a1f0ccfb1
Shawn Smith <sds.smith24@gmail.com>
2022-03-26 17:38:33 -0400

    add logic to generate random password based on user defined constraints

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 
-const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const num = "0123456789";
-const special = "!@#$%^&*_-+=";
+const setPicklist = (includeNum, includeSpec) => {
+    const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    const num = "0123456789".split('')
+    const special = "!@#$%^&*_-+=".split('')
 
-const minLength = document.getElementById('min-length').value
-const maxLength = document.getElementById('max-length').value
-const includeNum = document.getElementById('include-num').value
-const includeSpec = document.getElementById('include-spec').value
+    const list = alpha
+    includeNum && list.push(...num)
+    includeSpec && list.push(...special)
+    return list
+}
 
-const pwLength = (minLength, maxLength) => Math.floor(Math.random() * (max - min + 1)) + min
+const setPasswordElement = (password) => {
+    const pwContainer = document.getElementById('password-container')
+    const passwordElement = document.createElement('h2')
+    passwordElement.innerHTML = password
+    pwContainer.appendChild(passwordElement)
+}
 
-let password = ''
+const generatePassword = () => {
+    
+    const minLength = document.getElementById('min-length').value ? document.getElementById('min-length').value : 8
+    const maxLength = document.getElementById('max-length').value ? document.getElementById('max-length').value : 16
+    const includeNum = document.getElementById('include-num').checked
+    const includeSpec = document.getElementById('include-spec').checked
+    
+    console.log(minLength)
+
+    const pwLength = Math.floor(minLength) + Math.floor(Math.random() * (maxLength - minLength)) 
+    const pickList = setPicklist(includeNum, includeSpec)
+    console.log(pwLength)
+    let password = ''
+    for (let i = 0; i < pwLength; i ++) {
+        let charIndex = Math.floor(Math.random() * pickList.length)
+        password = password.concat(pickList[charIndex])
+    }
+    
+    setPasswordElement(password)
+}
+

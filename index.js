d64fee420d35a04b295fefe11b7e21209e66f820
Shawn Smith <sds.smith24@gmail.com>
2022-03-26 18:15:33 -0400

    set html element for generated password

--- index.js
+++ index.js
@@ -11,24 +11,19 @@ const setPicklist = (includeNum, includeSpec) => {
 }
 
 const setPasswordElement = (password) => {
-    const pwContainer = document.getElementById('password-container')
-    const passwordElement = document.createElement('h2')
+    const passwordElement = document.getElementById('password-container').firstElementChild
     passwordElement.innerHTML = password
-    pwContainer.appendChild(passwordElement)
 }
 
-const generatePassword = () => {
-    
+const generatePassword = () => {    
     const minLength = document.getElementById('min-length').value ? document.getElementById('min-length').value : 8
     const maxLength = document.getElementById('max-length').value ? document.getElementById('max-length').value : 16
     const includeNum = document.getElementById('include-num').checked
     const includeSpec = document.getElementById('include-spec').checked
-    
-    console.log(minLength)
 
     const pwLength = Math.floor(minLength) + Math.floor(Math.random() * (maxLength - minLength)) 
     const pickList = setPicklist(includeNum, includeSpec)
-    console.log(pwLength)
+
     let password = ''
     for (let i = 0; i < pwLength; i ++) {
         let charIndex = Math.floor(Math.random() * pickList.length)
